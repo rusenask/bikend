d45e8b2eb965c873c89b6f2f3328800178eb6bbe
jrl53 <josericardoleonlee@gmail.com>
2015-12-05 20:17:53 +0100

    adding esri map

--- static/bikend/www/js/app.js
+++ static/bikend/www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers', 'esri.core', 'esri.map'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -32,11 +32,12 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     controller: 'AppCtrl'
   })
 
-  .state('app.search', {
-    url: '/search',
+  .state('app.map', {
+    url: '/map',
     views: {
       'menuContent': {
-        templateUrl: 'templates/search.html'
+        templateUrl: 'templates/map.html',
+        controller: 'MapCtrl'
       }
     }
   })
--- static/bikend/www/js/controllers.js
+++ static/bikend/www/js/controllers.js
@@ -53,4 +53,29 @@ angular.module('starter.controllers', [])
 })
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
+})
+
+.controller('MapCtrl', function($scope, $stateParams, esriLoader) {
+    // initial map settings
+        // initial map settings
+        $scope.map = {
+            center: {
+                lng: -122.6819,
+                lat: 45.5200
+            },
+            zoom: 13
+        };
+
+        // this example requires the extent module
+        // so let's get that once the map is loaded
+        $scope.mapLoaded = function(map) {
+            esriLoader.require('esri/geometry/Extent').then(function(Extent) {
+                // now that we have the Extent module, we can
+                // wire up the click handler for bookmark buttons
+                $scope.goToBookmark = function(bookmark) {
+                    var extent = new Extent(bookmark.extent);
+                    map.setExtent(extent);
+                };
+            });
+        };
 });
