0b21ce90a08c27707529672364bd1e46408e0d8c
Karolis Rusenas <karolis.rusenas@gmail.com>
2015-12-05 16:22:28 +0000

    initial structs for  http handler and http client

--- server.go
+++ server.go
@@ -1,2 +1,15 @@
 package main
 
+// Client structure to be injected into functions to perform HTTP calls
+type Client struct {
+	HTTPClient *http.Client
+}
+
+// HTTPClientHandler used for passing http client connection and template
+// information back to handlers, mostly for testing purposes
+type HTTPClientHandler struct {
+	http Client
+	r    *render.Render
+	db   MongoDatabase
+}
+
