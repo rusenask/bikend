832eaf5b94c52264ef3ef24c4923987704403306
Karolis Rusenas <karolis.rusenas@gmail.com>
2015-12-05 16:22:58 +0000

    main server init

--- server.go
+++ server.go
@@ -13,6 +13,72 @@ type HTTPClientHandler struct {
 	db   MongoDatabase
 }
 
+
+func main() {
+	// Output to stderr instead of stdout, could also be a file.
+	log.SetOutput(os.Stderr)
+	log.SetFormatter(&log.TextFormatter{})
+
+	port := flag.String("port", ":80", "application port")
+	flag.Parse()
+
+	// geting db settings
+	initSettings()
+
+	// getting Mongo connection
+	// mongoAddress can be a list of master/slaves.
+	// session, err := mgo.Dial("server1.example.com,server2.example.com")
+	session, err := mgo.Dial(AppConfig.mongoAddress)
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	// Optional. Switch the session to a monotonic behavior. Monotonic consistency will start reading from
+	// a slave if possible, so that the load is better distributed, and once the first write happens the
+	// connection is switched to the master. This offers consistent reads and writes,
+	// but may not show the most up-to-date data on reads which precede the first write.
+	session.SetMode(mgo.Monotonic, true)
+
+	// ensuring indexes for name and category keywords
+	c := session.DB(AppConfig.databaseName).C("u_category")
+	index := mgo.Index{
+		Key: []string{"$text:name", "$text:keywords"},
+	}
+
+	err = c.EnsureIndex(index)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"Error": err.Error(),
+		}).Error("Failed to ensure full-text search indexes for u_category collection!")
+	} else {
+		log.Info("Indexes for u_category collection ensured!")
+
+		// app starting
+		log.WithFields(log.Fields{
+			"mongoAddress":            AppConfig.mongoAddress,
+			"databaseName":            AppConfig.databaseName,
+		}).Info("app is starting")
+
+		// getting base template and handler struct
+		r := render.New()
+
+		// getting database struct
+		database := MongoDatabase{s: session}
+
+		h := HTTPClientHandler{http: Client{&http.Client{}},
+			r:         r,
+			db:        database,
+		}
+
+		mux := getBoneRouter(h)
+		n := negroni.Classic()
+		n.Use(negronilogrus.NewMiddleware())
+		n.UseHandler(mux)
+		n.Run(*port)
+	}
+}
+
 func getBoneRouter(h HTTPClientHandler) *bone.Mux {
 	mux := bone.New()
 	// add new users
