0dba8a266e959e80c3744f81146439484573a80e
Juan Giorello <juangiorello@gmail.com>
2015-12-06 02:15:18 +0000

    Facebook login

--- static/bikend/www/js/controllers.js
+++ static/bikend/www/js/controllers.js
@@ -1,6 +1,6 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', [ 'ngOpenFB', 'ionic'])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $ionicLoading,$timeout, $window, ngFB) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -19,41 +19,7 @@ angular.module('starter.controllers', [])
     $scope.modal = modal;
   });
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
-
-// .controller('PlaylistsCtrl', function($scope) {
-//   $scope.playlists = [
-//     { title: 'Reggae', id: 1 },
-//     { title: 'Chill', id: 2 },
-//     { title: 'Dubstep', id: 3 },
-//     { title: 'Indie', id: 4 },
-//     { title: 'Rap', id: 5 },
-//     { title: 'Cowbell', id: 6 }
-//   ];
-// })
-
-.controller('UserCtrl', function($scope, $stateParams) {
-        $scope.playlist = {
+  $scope.userProfile = {
           user : {
             name: "Marty McFly",
             description: "Byke enthusiastic",
@@ -79,32 +45,122 @@ angular.module('starter.controllers', [])
               }
             ]
         };
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  $scope.showLoading = function() {
+          $ionicLoading.show({
+            template: 'Loading...'
+          });
+        };
+  $scope.hideLoading = function(){
+    $ionicLoading.hide();
+  };
+
+  // // Perform the login action when the user submits the login form
+  // $scope.doLogin = function() {
+  //   console.log('Doing login', $scope.loginData);
+
+  //   // Simulate a login delay. Remove this and replace with your login
+  //   // code if using a login system
+  //   $timeout(function() {
+  //     $scope.closeLogin();
+  //   }, 1000);
+  // };
+
+  
+
+  $scope.fbLogin = function () {
+      $scope.showLoading();
+      ngFB.login({scope: 'email,public_profile'}).then(
+          function (response) {
+              if (response.status === 'connected') {
+                  console.log('Facebook login succeeded');
+                  $scope.closeLogin();
+
+                  $scope.hideLoading();
+
+                  ngFB.api({
+                          path: '/me',
+                          params: {fields: 'id,name, last_name,first_name,email,gender,picture'}
+                      }).then(
+                          function (user) {
+                              $scope.user = user;
+
+                              $scope.userProfile.user.name = user.name;
+                              $scope.userProfile.user.description = user.email;
+                              $scope.userProfile.user.image = user.picture.data.url;
+                              $window.location.reload(true)
+
+                              console.log('Thanks for logging in, ' + user.name + '!' +user.id + ' ' +  user.email + ' ' + user.gender + ' ' +user.first_name + ' ' + user.last_name + ' ' + user.picture);
+                          },
+                          function (error) {
+                              alert('Facebook error: ' + error.error_description);
+                          });
+
+              } else {
+                  alert('Facebook login failed');
+              }
+          });
+  };
+
+
+
+
+})
+
+// .controller('PlaylistsCtrl', function($scope) {
+//   $scope.playlists = [
+//     { title: 'Reggae', id: 1 },
+//     { title: 'Chill', id: 2 },
+//     { title: 'Dubstep', id: 3 },
+//     { title: 'Indie', id: 4 },
+//     { title: 'Rap', id: 5 },
+//     { title: 'Cowbell', id: 6 }
+//   ];
+// })
+
+.controller('UserCtrl', function($scope, $stateParams, ngFB) {
+        
+        $scope.isLogged = false;
+
+        $scope.showLoading();
+        ngFB.api({
+            path: '/me',
+            params: {fields: 'id,name, last_name,first_name,email,gender,picture'}
+        }).then(
+            function (user) {
+                $scope.hideLoading();
+                $scope.user = user;
+                $scope.isLogged = true;
+                $scope.userProfile.user.name = user.name;
+                $scope.userProfile.user.description = user.email;
+                $scope.userProfile.user.image = user.picture.data.url;
+                              
+                $scope.justtry = $scope.userProfile
+                console.log('User Data in, ' + user.name + '!' +user.id + ' ' +  user.email + ' ' + user.gender + ' ' +user.first_name + ' ' + user.last_name + ' ' + user.picture);
+            },
+            function (error) {
+            $scope.hideLoading();
+                // alert('Facebook error: ' + error.error_description);
+            });
+
+
 })
 
 .controller('CreatePlaceCtrl', function($scope, $ionicModal, $timeout) {
 
   $scope.createPlace = {};
 
-    // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/my_place.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-
-
-  // Triggered in the login modal to close it
-  $scope.closeCreate = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.createPlace = function() {
-    $scope.modal.show();
-  };
-
-    // Perform the login action when the user submits the login form
-  $scope.doCreatePlace = function() {
+   $scope.doCreatePlace = function() {
     console.log('Doing doCreatePlace', $scope.loginData);
 
     // Simulate a login delay. Remove this and replace with your login
